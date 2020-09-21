1bd0d2027a60ea0207f0970e5d9a5608d30af7ce
Arjie Redoña <redonaarjie@pop-os.localdomain>
2020-09-21 12:23:35 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+/*  
+    Palindrome Logic 
+    --Get the reverse of the input number or string.
+    --Compare both the numbers and strings - reverse the number or string. 
+    --if the input and reverse are found to be same or equal, it means it is a palindrome.
+*/
+
+?>
+
+<?php 
+    // Check for Palindrome string using strrev() built-in function
+    function CheckPalindrome($string){   
+        if (strrev($string) == $string){   
+            return 1;   
+        } 
+        else{ 
+            return 0; 
+        } 
+    }  
+
+    $inputStringHere = "DAD";
+
+    if (CheckPalindrome($inputStringHere)) {
+        echo "<h1 style='text-align:center; color:blue;'>$inputStringHere - This Word is a Palindrome!</h1>";
+    } else {
+        echo "<h1 style='text-align:center; color:red;'>$inputStringHere - This Word is NOT a Palindrome!</h1>";
+    }
+
+?>
